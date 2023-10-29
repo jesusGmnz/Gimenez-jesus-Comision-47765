e338aeaf44c8a59af3abb151759d9629212be20d
Jesus <gimenezjesus18@gmail.com>
2023-10-29 14:59:09 -0300

    yacasicasi

--- appweb/urls.py
+++ appweb/urls.py
@@ -8,16 +8,20 @@ urlpatterns = [
     #el inicio de la pagina
     path('inicio',inicio,name="Inicio"),
     path('about',about,name="about"),
+    
+    #buscar los productos creados
     path('buscar/', Buscar_Producto),
     
     #login inicio de sesion
     path('login',login_request, name="Login"),
     path('logout', LogoutView.as_view(template_name='appweb/Autenticar/logout.html'), name="Logout"),
     path("register",register, name="Register"),
+    path("EditarUsuario", EditarUsuario, name="EditarUsuario"),
     
-    #productos
-    path("AgregarProducto",registroProducto, name="AgregarProducto"),
-    path("Productos", producto , name="Productos"),
-    path("EditarProductos/<producto_nombre>", editarProducto, name="EditarProductos"),
-    path("BorrarProductos/<producto_nombre>", eliminarProducto, name="BorrarProductos"),
+    #productos CRUD
+    path("AgregarProducto",registroProducto, name="AgregarProducto"), #crear un producto
+    path("Productos", producto , name="Productos"), #ver los productos creados
+    path("EditarProductos/<producto_nombre>", editarProducto, name="EditarProductos"), #modificar los productos
+    path("BorrarProductos/<producto_nombre>", eliminarProducto, name="BorrarProductos"), #eliminar algun producto
+    
 ]
